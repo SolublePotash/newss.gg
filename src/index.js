f44c550010ae48dc40b9d638cf23f6cc821a236b
SolublePotash <162484667+SolublePotash@users.noreply.github.com>
2024-09-06 20:55:23 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 app.use(
   '/',
   createProxyMiddleware({
-    target: 'https://holyubofficial.net',
+    target: 'https://bing.com',
     changeOrigin: true,
     ws: true,
     secure: false,
